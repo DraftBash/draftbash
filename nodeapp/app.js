146df5752faf25078bd34cb25c13eda070cf1b99
StephenFeddes <feddessteve@gmail.com>
2024-09-16 00:55:08 -0500

    Testing CICD.

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('API for DraftBash');
+    res.send('Testing API!');
 });
 
 app.listen(80, () => {
-    console.log('Server is running on port 80');
+    console.log('Server is running!');
 });
